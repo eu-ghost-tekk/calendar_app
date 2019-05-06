ca810cf5ef44adb3d4b9e2f947fb74627af5cbac
eu-ghost-tekk <idioteque.sub1@gmail.com>
2019-05-06 21:43:38 +0900

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
+
 //= require moment
 //= require fullcalendar
 //= require fullcalendar/lang/ja
